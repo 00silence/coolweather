a07f7f14deab5c90abb558905ab7dc1a87eeb34f
silence <silence@gmail.com>
2017-12-29 17:20:15 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by DELL on 2017/12/28/028.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
